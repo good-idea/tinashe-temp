7ae481f9880cb0e29bd5f13900a18c70b104b535
Joseph Thomas <joseph@good-idea.studio>
2019-08-16 16:57:44 -0700

    feat(app): set up initial theme

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,16 +1,32 @@
 import * as React from 'react'
+import * as createSanityClient from '@sanity/client'
 import { Router } from '@reach/router'
+import { ThemeProvider } from '@xstyled/styled-components'
 import { Home } from './views/Home'
-import { Track } from './views/Track'
+import { TrackDetails } from './views/TrackDetails'
 import { SiteDataProvider } from './context/SiteData'
+import { Navigation } from './views/Navigation'
+import { theme, GlobalStyles } from './theme'
+
+const client = createSanityClient({
+  projectId: 'eiufuzvv',
+  dataset: 'production',
+  useCDN: true,
+})
 
 export const App = () => {
   return (
-    <SiteDataProvider>
-      <Router>
-        <Home path="/" />
-        <Track path="/tracks/:trackName" />
-      </Router>
-    </SiteDataProvider>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <SiteDataProvider client={client}>
+          <Navigation />
+          <Router>
+            <Home path="/" />
+            <TrackDetails path="/tracks/:trackName" />
+          </Router>
+        </SiteDataProvider>
+      </>
+    </ThemeProvider>
   )
 }
---
+++ app/src/theme/index.ts
@@ -0,0 +1,9 @@
+export * from './global'
+
+export const theme = {
+  fontSizes: [42, 36, 24, 16, 11],
+  fonts: {
+    sans:
+      "'Monument Grotesk', Verdana, Geneva, Helvetica Neue, Helvetica, sans-serif",
+  },
+}
